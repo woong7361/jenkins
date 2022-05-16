04b670e944817062fedcc754f97908bded0e7447
woong7361 <woong7361@gmail.com>
2022-05-16 12:11:51 +0900

    1

---
+++ src/main/java/com/example/awsstudy/AwsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsStudyApplication.class, args);
+    }
+
+}
