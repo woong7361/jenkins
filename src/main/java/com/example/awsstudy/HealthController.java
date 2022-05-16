4434bb7b8c0f825fbaa42d4eb87ffa15144f037c
woong7361 <woong7361@gmail.com>
2022-05-16 16:03:42 +0900

    9

--- src/main/java/com/example/awsstudy/HealthController.java
+++ src/main/java/com/example/awsstudy/HealthController.java
@@ -1,13 +1,16 @@
 package com.example.awsstudy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthController {
 
     @GetMapping
     public String healthCheck(){
+        log.info("123123123123");
         return "떳다!";
     }
 }
