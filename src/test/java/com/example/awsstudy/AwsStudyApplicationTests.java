57f332bd9c8358632fd8a1459c6b068c6e90b21b
woong7361 <woong7361@gmail.com>
2022-05-16 20:06:09 +0900

    14

--- src/test/java/com/example/awsstudy/AwsStudyApplicationTests.java
+++ src/test/java/com/example/awsstudy/AwsStudyApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.awsstudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AwsStudyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class AwsStudyApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
